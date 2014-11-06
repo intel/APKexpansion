d684ca83e7caba89d68b3aef3c5edf6748bdfb9e
Nazia Latif <nazia.latif@intel.com>
2014-11-06 15:32:39 -0500

    Adding ant build files for crosswalk builds

---
+++ AndroidLibrary/GoogleExtras/play_licensing/library/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\nlatif\Desktop\Containers\adt-bundle-windows-x86_64-20131030\sdk\extras\google\play_licensing\library\ant-build\AndroidManifest.xml : \
+C:\Users\nlatif\Desktop\Containers\adt-bundle-windows-x86_64-20131030\sdk\extras\google\play_licensing\library\AndroidManifest.xml \
